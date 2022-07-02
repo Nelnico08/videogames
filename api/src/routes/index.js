6f879f5d7921d76779b53e358d154d4959cc3c5a
Nelnico <nelnico008@gmail.com>
2022-07-02 01:19:31 -0300

    database done and start of routes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const { API_KEY } = process.env;
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -7,6 +8,12 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+router.get("/videogames",  (req, res) => {
+    const videogames = fetch(`https://api.rawg.io/api/games?key=${API_KEY}`)
+        .then(game => game.json());    
+    
+    res.json(videogames)
+})
 
 
 module.exports = router;
