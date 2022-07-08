c3c3fc6c04108594ad7952259cf6960d6b6db6d2
Nelnico <nelnico008@gmail.com>
2022-07-07 22:58:21 -0300

    add genre and platform arrays, finish navbar logic and estar with form

--- client/src/Components/CreateGame.jsx
+++ client/src/Components/CreateGame.jsx
@@ -1,45 +1,105 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { Link, useHistory } from 'react-router-dom';
+import { createNewGame } from '../Redux/actions';
+import { all_genres, all_platforms } from '../Genres_Platforms/GenresPlatforms';
 // import style from '../Styles/CreateGame.css';
 
 export default function CreateGame() {
+  const [newVideogame, setNewVideogame] = useState({
+    name: '',
+    released: '',
+    rating: 0,
+    genres: [],
+    platforms: [],
+    description: '',
+  });
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const handleOnChange = (e) => {
+    e.preventDefault();
+    setNewVideogame({ name: e.target.value });
+  };
+
   return (
-    <form>
-      <h2>Post new videogame</h2>
-      <label>Name: </label>
-      <input type='text' placeholder='Name'/>
-      <label>Released: </label>
-      <input type='date'/>
-      <label>Rating: </label>
-      <input type='number' placeholder='between 1-5'/>
-      <label>Genres: </label>
-      <select multiple value='Genres'>
-          <option value='Action'>Action</option>
-          <option value='Indie'>Indie</option>
-          <option value='Adventure'>Adventure</option>
-          <option value='RPG'>RPG</option>
-          <option value='Strategy'>Strategy</option>
-          <option value='Shooter'>Shooter</option>
-          <option value='Casual'>Casual</option>
-          <option value='Simulation'>Simulation</option>
-          <option value='Puzzle'>Puzzle</option>
-          <option value='Arcade'>Arcade</option>
-          <option value='Platformer'>Platformer</option>
-          <option value='Racing'>Racing</option>
-          <option value='Massively Multiplayer'>Massively Multiplayer</option>
-          <option value='Sports'>Sports</option>
-          <option value='Fighting'>Fighting</option>
-          <option value='Family'>Family</option>
-          <option value='Board Games'>Board Games</option>
-          <option value='Educational'>Educational</option>
-          <option value='Card'>Card</option>
-      </select>
-      <label>Platforms: </label>
-      <select multiple>
-        <option value='despues sigo'>despues sigo</option>
-      </select>
-      <label>Description: </label>
-      <input type='text' placeholder='Description...'/>
-      <button type='submit'>Post new videogame</button>
-    </form>
-  )
+    <div>
+      <Link to="/home">
+        <button>Back to home</button>
+      </Link>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          // dispatch(createNewGame(newVideogame));
+          history.push('/home');
+        }}
+      >
+        <h2>Post new videogame</h2>
+        <label>Name: </label>
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={newVideogame.name}
+          onChange={handleOnChange}
+        />
+        <label>Released: </label>
+        <input
+          type="date"
+          name="released"
+          value={newVideogame.released}
+          onChange={handleOnChange}
+        />
+        <label>Rating: </label>
+        <input
+          type="number"
+          placeholder="between 1-5"
+          name="rating"
+          value={newVideogame.rating}
+          onChange={handleOnChange}
+        />
+        <div>
+          <label>Genres: </label>
+          {/*select con 19 opciones de generos */}
+          <select
+            multiple
+            name="genres"
+            value={newVideogame.genres}
+            onChange={handleOnChange}
+          >
+            {all_genres.map((genre) => (
+              <option value={genre}>{genre}</option>
+            ))}
+          </select>
+          <ul>
+            {newVideogame.genres?.map((genre) => (
+              <li>{genre}</li>
+            ))}
+          </ul>
+        </div>
+
+        <label>Platforms: </label>
+        {/*select con 12 opciones de plataformas */}
+        <select
+          multiple
+          name="platforms"
+          value={newVideogame.platforms}
+          onChange={handleOnChange}
+        >
+          {all_platforms.map((platform) => (
+            <option value={platform}>{platform}</option>
+          ))}
+        </select>
+        <label>Description: </label>
+        <input
+          type="text"
+          placeholder="Description..."
+          name="description"
+          value={newVideogame.description}
+          onChange={handleOnChange}
+        />
+        <button type="submit">Post new videogame</button>
+      </form>
+    </div>
+  );
 }
--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { all_genres } from '../Genres_Platforms/GenresPlatforms';
 import SearchBar from './SearchBar';
 // import style from '../Styles/NavBar.css'
 
@@ -19,25 +20,9 @@ export default function NavBar({
         {/*select con 20 options => 19generos y all games */}
         <select onChange={(e) => genreHandler(e)}>
           <option value="All Genres">All Genres</option>
-          <option value="Action">Action</option>
-          <option value="Indie">Indie</option>
-          <option value="Adventure">Adventure</option>
-          <option value="RPG">RPG</option>
-          <option value="Strategy">Strategy</option>
-          <option value="Shooter">Shooter</option>
-          <option value="Casual">Casual</option>
-          <option value="Simulation">Simulation</option>
-          <option value="Puzzle">Puzzle</option>
-          <option value="Arcade">Arcade</option>
-          <option value="Platformer">Platformer</option>
-          <option value="Racing">Racing</option>
-          <option value="Massively Multiplayer">Massively Multiplayer</option>
-          <option value="Sports">Sports</option>
-          <option value="Fighting">Fighting</option>
-          <option value="Family">Family</option>
-          <option value="Board Games">Board Games</option>
-          <option value="Educational">Educational</option>
-          <option value="Card">Card</option>
+          {all_genres.map((genre) => (
+            <option value={genre}>{genre}</option>
+          ))}
         </select>
         {/*select con 3 options */}
         <select onChange={(e) => createdGameHandler(e)}>
