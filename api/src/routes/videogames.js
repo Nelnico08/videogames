daf19a7eab0f816c2d5b421f6d8f74a6b3775ad6
Nelnico <nelnico008@gmail.com>
2022-07-23 11:46:05 -0300

    add icon and change some styles

--- api/src/routes/videogames.js
+++ api/src/routes/videogames.js
@@ -6,121 +6,106 @@ const { API_KEY } = process.env;
 const axios = require('axios');
 const { Videogame, Genre } = require('../db');
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   const { name } = req.query;
 
-  if (name) {
-    let allVideogames = [];
-    Videogame.findAll({
-      where: {
-        name: { [Op.iLike]: `%${name}%` },
-      },
-      include: Genre,
-      limit: 15,
-    })
-      .then((dbGamesSearch) => {
-        if (dbGamesSearch.length) {
-          allVideogames = dbGamesSearch?.map((elem) => {
-            return {
-              id: elem.id,
-              name: elem.name,
-              released: elem.released,
-              image: elem.image,
-              rating: elem.rating,
-              platforms: elem.platforms?.map((plat) => plat),
-              genres: elem.genres?.map((genre) => genre.name),
-            };
-          });
-          return allVideogames;
-        }
-      })
-      .then(() => {
-        axios
-          .get(`https://api.rawg.io/api/games?search=${name}&key=${API_KEY}`)
-          .then((resp) => resp.data.results)
-          .then((apiGamesSearch) => {
-            if (apiGamesSearch.length) {
-              apiGamesSearch.forEach((elem) => {
-                allVideogames.push({
-                  id: elem.id,
-                  name: elem.name,
-                  released: elem.released,
-                  image: elem.background_image,
-                  rating: elem.rating,
-                  platforms: elem.platforms?.map((plat) => plat.platform.name),
-                  genres: elem.genres?.map((genre) => genre.name),
-                });
-              });
-              return allVideogames;
-            }
-          })
-          .then(() => {
-            if (!allVideogames.length) {
-              return res.send("Can't find game");
-            }
-            allVideogames = allVideogames.slice(0, 15);
-
-            res.send(allVideogames);
-          });
-      })
-      .catch((err) => res.status(404).send(err.message));
-  } else {
-    let videogames = [];
-
-    Videogame.findAll({
-      include: Genre,
-      limit: 100,
-    })
-      .then((allDBGames) => {
-        if (allDBGames.length) {
-          videogames = allDBGames.map((elem) => {
-            return {
-              id: elem.id,
-              name: elem.name,
-              released: elem.released,
-              image: elem.image,
-              rating: elem.rating,
-              platforms: elem.platforms.map((plat) => plat),
-              genres: elem.genres.map((genre) => genre.name),
-            };
-          });
-        }
-        return videogames;
-      })
-      .then(() => {
-        let count = 0;
-
-        let pagePetition = function (response) {
-          let { data } = response;
-          videogames.push(
-            ...data.results.map((game) => {
-              return {
-                id: game.id,
-                name: game.name,
-                released: game.released,
-                image: game.background_image,
-                rating: game.rating,
-                platforms: game.platforms.map((plat) => plat.platform.name),
-                genres: game.genres.map((genre) => genre.name),
-              };
-            })
-          );
-
-          count++;
-          if (count < 5) {
-            axios.get(data.next).then(pagePetition);
-          } else {
-            return res.send(videogames);
-          }
-        };
-
-        axios
-          .get(`https://api.rawg.io/api/games?key=${API_KEY}`)
-          .then(pagePetition);
-      })
-      .catch((err) => {
-        return res.status(404).send(err.message);
+  try {
+    if (name) {
+      let videogameSearch = [];
+      const dbGamesSearch = await Videogame.findAll({
+        where: { name: { [Op.iLike]: `%${name}%` } },
+        include: Genre,
+        limit: 15,
       });
+      if (dbGamesSearch.length) {
+        videogameSearch = dbGamesSearch.map((elem) => {
+          return {
+            id: elem.id,
+            name: elem.name,
+            released: elem.released,
+            image: elem.image,
+            rating: elem.rating,
+            platforms: elem.platforms?.map((plat) => plat),
+            genres: elem.genres?.map((genre) => genre.name),
+          };
+        });
+      }
+      const apiGameSearch = (
+        await axios.get(
+          `https://api.rawg.io/api/games?search=${name}&key=${API_KEY}`
+        )
+      ).data.results;
+      if (apiGameSearch.length) {
+        apiGameSearch.forEach((elem) => {
+          videogameSearch.push({
+            id: elem.id,
+            name: elem.name,
+            released: elem.released,
+            image: elem.background_image,
+            rating: elem.rating,
+            platforms: elem.platforms?.map((plat) => plat.platform.name),
+            genres: elem.genres?.map((genre) => genre.name),
+          });
+        });
+      }
+      if (!videogameSearch.length) {
+        return res.send("Can't find game");
+      }
+      videogameSearch = videogameSearch.slice(0, 15);
+
+      return res.send(videogameSearch);
+    } else {
+      let videogame = [];
+
+      const allDBGames = await Videogame.findAll({
+        unclude: Genre,
+        limit: 100,
+      });
+      if (allDBGames.length) {
+        videogame = allDBGames.map((elem) => {
+          return {
+            id: elem.id,
+            name: elem.name,
+            released: elem.released,
+            image: elem.image,
+            rating: elem.rating,
+            platforms: elem.platforms.map((plat) => plat),
+            genres: elem.genres.map((genre) => genre.name),
+          };
+        });
+      }
+      let count = 0;
+
+      let pagePetition = function (response) {
+        let { data } = response;
+        videogame.push(
+          ...data.results.map((game) => {
+            return {
+              id: game.id,
+              name: game.name,
+              released: game.released,
+              image: game.background_image,
+              rating: game.rating,
+              platforms: game.platforms.map((plat) => plat.platform.name),
+              genres: game.genres.map((genre) => genre.name),
+            };
+          })
+        );
+
+        count++;
+        if (count < 5) {
+          axios.get(data.next).then(pagePetition);
+        } else {
+          return res.send(videogame);
+        }
+      };
+
+      axios
+        .get(`https://api.rawg.io/api/games?key=${API_KEY}`)
+        .then(pagePetition);
+    }
+  } catch (err) {
+    res.status(404).send(err.message);
   }
 });
 
--- client/src/Components/Landing.jsx
+++ client/src/Components/Landing.jsx
@@ -5,7 +5,7 @@ import style from '../Styles/Landing.module.css';
 export default function Landing() {
   return (
     <div className={style.content}>
-      <h1 className={style.welcome}>Henry Videogames</h1>
+      <h1 className={style.welcome}>Welcome</h1>
       <Link to="Home">
         <button className={style.startButton}>Start</button>
       </Link>
