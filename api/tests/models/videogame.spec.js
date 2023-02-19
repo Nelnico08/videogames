4f16fc2434acb7fb08c117c1f8c71feffa8f2380
Nelnico <nelnico008@gmail.com>
2023-02-18 21:13:07 -0300

    add vercel.json in api

--- api/tests/models/videogame.spec.js
+++ api/tests/models/videogame.spec.js
@@ -3,6 +3,7 @@ const { expect } = require('chai');
 
 describe('Videogame model', () => {
   before(() => conn.authenticate()
+    .then(() => done())
     .catch((err) => {
       console.error('Unable to connect to the database:', err);
     }));
@@ -14,9 +15,23 @@ describe('Videogame model', () => {
           .then(() => done(new Error('It requires a valid name')))
           .catch(() => done());
       });
-      it('should work when its a valid name', () => {
-        Recipe.create({ name: 'Super Mario Bros' });
-      });
+      // it('should work when its a valid name', () => {
+      //   Recipe.create({ name: 'Super Mario Bros' });
+      // });
     });
+    describe('description', () => {
+      it('should throw an error if description is null', (done) => {
+        Videogame.create({name: 'Super Mario Bros'})
+          .then(() => done(new Error('It requires a description')))
+          .then(done())
+      })
+    });
+    describe('platforms', () => {
+      it('should throw an error if platforms is null', (done) => {
+        Videogame.create({name: 'Super Mario Bros', description: 'a description'})
+          .then(() => done(new Error('It requires at least one platform')))
+          .then(done())
+      })
+    }) 
   });
 });
